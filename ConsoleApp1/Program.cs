b8d602e5e4fa7595b98b4e8ebdf335fde276c45c
Sin <103442056+Sinzxc@users.noreply.github.com>
2023-03-11 16:13:16 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,14 @@
-﻿
+
 using System;
 using System.ComponentModel;
 using System.Security.AccessControl;
 
-class Point
+interface IComparable
+{
+    public bool Equals(object p);
+    public int GetHashCode();
+}
+class Point:IComparable
 {
     public int X { get; protected set; }
     public int Y { get; protected set; }
@@ -26,15 +31,30 @@ class Point
 
     public override string ToString()
     {
-        return $"[{X} : {Y}]\n";
+        return $"({X} : {Y})\n";
     }
 
-    public bool Equals(Point p)
+    public override bool Equals(object p)
     {
-        if (this.X == p.X && this.Y == p.Y)
+        Point temp = (Point)p;
+        if (this.X == temp.X && this.Y == temp.Y)
             return true;
         else
             return false;
+    }
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public static bool operator ==(Point a, Point b)
+    {
+       return a.Equals(b);
+    }
+
+    public static bool operator !=(Point a, Point b)
+    {
+        return !(a.Equals(b));
     }
     public static Point operator +(Point a, Point b)
     {
@@ -45,6 +65,10 @@ class Point
 class ColoredPoint:Point
 {
     private string Color;
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
 
     public ColoredPoint() : base()
     {
@@ -64,14 +88,24 @@ class ColoredPoint:Point
     }
     public override string ToString()
     {
-        return $"[{X} : {Y}] Color= {Color}\n";
+        return $"({X} : {Y}) Color= {Color}\n";
     }
-    public bool Equals(ColoredPoint p)
+    public override bool Equals(object p)
     {
-        if (this.X == p.X && this.Y == p.Y&&this.Color==p.Color)
+        ColoredPoint temp = (ColoredPoint)p;
+        if (this.X == temp.X && this.Y == temp.Y&&this.Color == temp.Color)
             return true;
         else
             return false;
+    }
+    public static bool operator ==(ColoredPoint a, ColoredPoint b)
+    {
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(ColoredPoint a, ColoredPoint b)
+    {
+        return !(a.Equals(b));
     }
 }
 
@@ -79,6 +113,15 @@ class MultiAngle:Point
 {
     protected Point[] Points = new Point[100];
     protected int Angles = 0;
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+    public MultiAngle()
+    {
+        Points[0] = new Point(0, 0);
+        Angles = 1;
+    }
     public MultiAngle(params Point [] p)
     {
         Array.Copy(p,Points, p.Length);
@@ -97,21 +140,56 @@ class MultiAngle:Point
 
        return s;
     }
-    public bool Equals(MultiAngle p)
+    public override bool Equals(object p)
     {
-        bool Check = true; 
-        for (int i = 0; i < Angles; i++)
+        bool Check = true;
+        MultiAngle temp = (MultiAngle)p;
+        if (Angles == temp.Angles)
         {
-            if (!this.Points[i].Equals(p.Points[i]))
-                Check = false;
+            for (int i = 0; i < Angles; i++)
+            {
+                if (!this.Points[i].Equals(temp.Points[i]))
+                    Check = false;
+            }
         }
         return Check;
+    }
+    public static bool operator ==(MultiAngle a, MultiAngle b)
+    {
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(MultiAngle a, MultiAngle b)
+    {
+        return !(a.Equals(b));
+    }
+
+    public void MoveToX(int x)
+    {
+        for (int i = 0; i < Angles; i++)
+            Points[i] = new Point(Points[i].X + x, Points[i].Y);
+    }
+
+    public void MoveToY(int y)
+    {
+        for (int i = 0; i < Angles; i++)
+            Points[i] = new Point(Points[i].X, Points[i].Y+y);
+    }
+
+    public void Move(int x, int y)
+    {
+        for (int i = 0; i < Angles; i++)
+            Points[i] = new Point(Points[i].X + x, Points[i].Y+y);
     }
 }
 
 class ColoredLine:MultiAngle
 {
     private string Color;
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
     public ColoredLine(params Point[] p)
     {
         Array.Copy(p, Points, p.Length);
@@ -136,18 +214,30 @@ class ColoredLine:MultiAngle
         s += "Color= " + Color;
         return s;
     }
-    public bool Equals(ColoredLine p)
+    public override bool Equals(object p)
     {
         bool Check = true;
-        for (int i = 0; i < Angles; i++)
+        ColoredLine temp = (ColoredLine)p;
+        if (Angles == temp.Angles)
         {
-            if (!this.Points[i].Equals(p.Points[i]))
-                Check = false;
+            for (int i = 0; i < Angles; i++)
+            {
+                if (!this.Points[i].Equals(temp.Points[i]))
+                    Check = false;
+            }
         }
-        if (this.Color != p.Color)
+        if (this.Color != temp.Color)
             Check = false;
-
         return Check;
+    }
+    public static bool operator ==(ColoredLine a, ColoredLine b)
+    {
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(ColoredLine a, ColoredLine b)
+    {
+        return !(a.Equals(b));
     }
 }
 
@@ -166,7 +256,7 @@ class HelloWorld
         MultiAngle l1 = new MultiAngle(p, p1);
         Console.WriteLine(l.ToString());
         Console.WriteLine(l1.ToString());
-        Console.WriteLine(l.Equals(l1));
+        Console.WriteLine(l==l1);
         Console.WriteLine("\n\n\nЦветная линия: ");
         ColoredLine cl = new ColoredLine("White",p,p1);
         Console.WriteLine(cl.ToString());
@@ -175,6 +265,9 @@ class HelloWorld
         Console.WriteLine(cp.ToString());
         Console.WriteLine("\n\n\nМногоугольник:");
         MultiAngle m = new MultiAngle(p, p1, new Point(1, 3));
+        Console.WriteLine(m.ToString());
+        Console.WriteLine("\n\n\nМногоугольник перемещён:");
+        m.Move(2, 2);
         Console.WriteLine(m.ToString());
     }
 }
